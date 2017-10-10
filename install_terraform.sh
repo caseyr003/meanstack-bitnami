503b294259706b4b27f7f41bfaf3b181fc9f7391
casey <caseyross003@gmail.com>
2017-10-10 11:39:46 -0500

    update install script

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,7 +1,16 @@
 # to install terraform
 version=terraform_0.10.6_linux_amd64.zip
 wget "https://releases.hashicorp.com/terraform/0.10.6/$version"
-unzip $version
+unzip $version -d ~/
 rm $version
 cd /usr/bin
 sudo ln -s $HOME/terraform terraform
+cd ~/Install-TerraForm
+
+wget "https://github.com/oracle/terraform-provider-oci/releases/download/v2.0.1/linux.tar.gz"
+tar -xvzf linux.tar.gz
+
+rm -rf linux_386/ linux_arm/ linux.tar.gz
+mkdir ~/.terraform.d
+mkdir ~/.terraform.d/plugins
+mv linux_amd64/ ~/.terraform.d/plugins/
